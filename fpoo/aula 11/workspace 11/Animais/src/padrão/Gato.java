1db274b1de0cb4d43ef591e036c2df99fc0dcf6f
Rodrigo Freire <111870583+rodrigodemelofreire@users.noreply.github.com>
2022-11-22 09:54:02 -0300

    Add files via upload

---
+++ fpoo/aula 11/workspace 11/Animais/src/padrão/Gato.java
@@ -0,0 +1,15 @@
+package padrão;
+
+public class Gato {
+	String nome;
+	String raca;
+	float peso;
+	
+	void miar() {
+		System.out.println("Miau");
+	}
+	
+	void ronronar() {
+		System.out.println("ronronron");
+	}
+}
